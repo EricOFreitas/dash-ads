b16535bf148c1ee1a96718a8d3b71d3edd5e1f4f
Eric Freitas <erics9@gmail.com>
2026-03-20 19:32:54 -0300

    intro a php

--- introphp.php
+++ introphp.php
@@ -1,6 +1,14 @@
 <?php
-    $usuario = $_POST['nome'];
-    $senha = $_POST['senha'];
-    
-    echo "Usuário " . $usuario . " e senha " . $senha . " cadastrados com sucesso!";
-?>
+    $n1 = 8.5;
+    $n2 = 6;
+    $m = ($n1+$n2)/2;
+   echo "A média é: ".$m;
+   echo "<br/>";
+    //verificar se passou de ano
+    if( $m < 7 ){
+        echo "Reprovado";
+    }else{
+        echo "Aprovado";
+    }
+   ?>
+
